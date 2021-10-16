583f214cc1f84a78868e048ad796f10b990c2064
Baha <bahako455@gmail.com>
2021-10-16 17:39:47 +0600

    Commit

--- app/src/main/java/com/example/bishkekrielt/MainActivity.kt
+++ app/src/main/java/com/example/bishkekrielt/MainActivity.kt
@@ -21,7 +21,6 @@ open class MainActivity : AppCompatActivity() {
             when (destination.id) {
 
                 R.id.navigation_home -> navView.visibility = View.VISIBLE
-                R.id.navigation_dashboard -> navView.visibility = View.VISIBLE
                 R.id.navigation_notifications -> navView.visibility = View.VISIBLE
                 else -> navView.visibility = View.GONE
             }
--- app/src/main/java/com/example/bishkekrielt/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/bishkekrielt/ui/notifications/NotificationsFragment.kt
@@ -4,16 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.bishkekrielt.R
-import com.example.bishkekrielt.data.common.BaseFragment
-import com.example.bishkekrielt.databinding.FragmentHomeBinding
 import com.example.bishkekrielt.databinding.FragmentNotificationsBinding
-import com.example.bishkekrielt.ui.dashboard.DashboardViewModel
-import com.example.bishkekrielt.ui.home.HomeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NotificationsFragment : Fragment() {
