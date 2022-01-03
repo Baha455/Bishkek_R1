f128a25b2bcf62a77a61d7ef2579be74f3f8c863
Baha <bahako455@gmail.com>
2022-01-03 17:27:37 +0600

    update

--- app/src/main/java/com/example/bishkekrielt/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/bishkekrielt/ui/home/HomeViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.bishkekrielt.ui.home
 
-import androidx.lifecycle.*
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bishkekrielt.data.model.Recatalog
 import com.example.bishkekrielt.data.repository.Repository
-import com.example.bishkekrielt.data.model.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -11,11 +14,16 @@ class HomeViewModel(private val service: Repository, savedStateHandle: SavedStat
     val catalog = MutableLiveData<List<Recatalog>>()
 
     private val _catalog = savedStateHandle.getLiveData("catalog", getData())
-    //val catalog1: MutableLiveData<Recatalog> = _catalog
+    //val catalog1: MutableLiveData<> = _catalog
+    /*val filteretData: LiveData<List<String>> =
+        savedStateHandle.getLiveData<String>("query").switchMap {  }*/
+
+
 
     fun getData(){
     viewModelScope.launch(Dispatchers.IO) {
         kotlin.runCatching {
+
             val rieltProjection = service.loadDescription()
             if (rieltProjection.isSuccessful) catalog.postValue(rieltProjection.body()?.catalog)
         }
